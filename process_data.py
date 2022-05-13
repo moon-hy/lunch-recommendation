9973c82d09667ae304ba33633186d6c556ae372e
hy.moon <moraramee@gmail.com>
2022-05-13 17:33:57 +0900

    added: process_data

--- process_data.py
+++ process_data.py
@@ -5,9 +5,8 @@ categories = {
     category: '/'.join(category.split('.')) for category in df.category.unique()
 }
 df.category = df.category.apply(lambda x: categories[x])
-df.drop_duplicates(inplace=True)
 
 df[['category']].drop_duplicates().to_csv('./data/categories.csv', index=False)
-df.to_csv('./data/foods.csv', index=False)
+df[['menu','category']].drop_duplicates().to_csv('./data/foods.csv', index=False)
 
 print('DONE: Processed menu_data.csv\'s categories.')
