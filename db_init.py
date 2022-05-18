04f5c7117a09ba94b656e71c60895f05f112833f
hy.moon <moraramee@gmail.com>
2022-05-18 11:03:51 +0900

    added: post category init

--- db_init.py
+++ db_init.py
@@ -8,10 +8,18 @@ import pandas as pd
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
 
-from feature.models import (
-    Category, Food
-)
+import feature.models as f
+import community.models as c
 
+
+def insert_posts_category():
+    categories = []
+    for title in ['질문', '추천', '자유']:
+        categories.append(c.Category(
+            name    = title
+        ))
+    c.Category.objects.bulk_create(categories)
+    print('DONE: Post Categories')
 
 CSV_PATH = './data/'
 def insert_category():
@@ -19,11 +27,11 @@ def insert_category():
     with open(CSV_PATH+'categories.csv', newline='', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            if not Category.objects.filter(name=row['category']).exists():
-                categories.append(Category(
+            if not f.Category.objects.filter(name=row['category']).exists():
+                categories.append(f.Category(
                     name    = row['category']
                 ))
-    Category.objects.bulk_create(categories)
+    f.Category.objects.bulk_create(categories)
     print('DONE: Insert Category data.')
 
 IMAGE_PATH = './data/images/'
@@ -38,16 +46,17 @@ def insert_food():
                 image       = ImageFile(open(IMAGE_PATH+name+'.png', 'rb'))
             except:
                 image       = ''
-            category        = Category.objects.get(name=row['category'])
-            if not Food.objects.filter(name=row['menu']).exists():
-                foods.append(Food(
+            category        = f.Category.objects.get(name=row['category'])
+            if not f.Food.objects.filter(name=row['menu']).exists():
+                foods.append(f.Food(
                     name    = name,
                     kcal    = kcal,
                     image   = image,
                     category= category
                 ))
-    Food.objects.bulk_create(foods)
+    f.Food.objects.bulk_create(foods)
     print('DONE: Insert Food data.')
 
+insert_posts_category()
 insert_category()
 insert_food()
