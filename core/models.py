d3444ba3d877b39699588a62c6ab7820818de692
hy.moon <moraramee@gmail.com>
2022-05-18 12:12:27 +0900

    added: index

--- core/models.py
+++ core/models.py
@@ -20,11 +20,18 @@ class TimeStampedModel(models.Model):
         abstract    = True
 
 class GetRequestLog(models.Model):
-    endpoint        = models.CharField(max_length=100, null=True)
+    endpoint        = models.CharField(max_length=100, null=True, db_index=True)
     user            = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     response_code   = models.PositiveSmallIntegerField()
     remote_address  = models.CharField(max_length=20, null=True)
     exec_time       = models.IntegerField(null=True)
-    date            = models.DateTimeField(auto_now=True)
+    date            = models.DateTimeField(auto_now=True, db_index=True)
     body_response   = models.TextField()
     body_request    = models.TextField()
+
+    class Meta:
+        db_table    = 'core_get_logs'
+        ordering    = ['-date']
+
+    def __str__(self):
+        return f'{self.date} | {self.endpoint}'
