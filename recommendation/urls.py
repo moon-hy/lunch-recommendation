613105fca86c935323a37fea825705511c754d59
hy.moon <moraramee@gmail.com>
2022-05-19 18:52:25 +0900

    added: interest-popular/user

--- recommendation/urls.py
+++ recommendation/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
 from recommendation.views import (
+    InterestPopularRecommend,
+    InterestUserRecommend,
     RandomRecommend,
     YesterdayPopularRecommend
 )
@@ -8,5 +10,7 @@ from recommendation.views import (
 
 urlpatterns = [
     path('random', RandomRecommend.as_view()),
-    path('yesterday-popular', YesterdayPopularRecommend.as_view())
+    path('yesterday-popular', YesterdayPopularRecommend.as_view()),
+    path('interest-popular', InterestPopularRecommend.as_view()),
+    path('interest-user', InterestUserRecommend.as_view()),
 ]
--- recommendation/views.py
+++ recommendation/views.py
@@ -20,10 +20,13 @@ from drf_yasg import openapi
 from feature.models import Food, History
 from feature.serializers import FoodDetailSerializer, FoodListSerializer
 
+
+User = get_user_model()
+
 class RandomRecommend(APIView):
     serializer_class    = FoodListSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (AllowAny, )
 
     @swagger_auto_schema(   
         operation_id            = '음식 추천 - 랜덤',
@@ -33,7 +36,7 @@ class RandomRecommend(APIView):
     def get(self, request):
         foods       = Food.objects.all()
         counts_foods= foods.count()
-        foods_chosen= sample(list(foods), k=min(10, counts_foods))
+        foods_chosen= sample(list(foods), k=min(5, counts_foods))
         serializer  = self.serializer_class(foods_chosen, many=True)
         return Response(serializer.data, status=HTTP_200_OK)
 
@@ -41,7 +44,7 @@ class RandomRecommend(APIView):
 class YesterdayPopularRecommend(APIView):
     serializer_class    = FoodListSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (AllowAny, )
 
     @swagger_auto_schema(   
         operation_id            = '음식 추천 - 인기',
@@ -59,6 +62,84 @@ class YesterdayPopularRecommend(APIView):
             count=Count('id')
         ).order_by('-count')
         
+        foods       = []
+        idx         = 0
+        
+        while len(foods)<10 and idx<len(histories):
+            food_id = histories[idx]['food']
+            try:
+                foods.append(Food.objects.get(pk=food_id))
+            except:
+                pass
+            idx += 1
+
+        serializer  = self.serializer_class(foods, many=True)
+        return Response(serializer.data, status=HTTP_200_OK)
+
+class InterestPopularRecommend(APIView):
+    serializer_class    = FoodListSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated, )
+
+    @swagger_auto_schema(   
+        operation_id            = '음식 추천 - 선호대분류 기반',
+        operation_description   = '선택한 선호 대분류에서 가장 인기있는 음식을 추천합니다.',
+        responses               = {200: openapi.Response('', FoodListSerializer)}
+    )
+    def get(self, request):
+        last_month  = datetime.date.today() - datetime.timedelta(days=30)
+        
+        histories   = History.objects.select_related(
+            'food__category'
+        ).all().values(
+            'food'
+        ).filter(
+            food__category =request.user.profile.interest_in
+        ).filter(
+            created_at__gte=last_month
+        ).annotate(
+            count=Count('id')
+        ).order_by('-count')
+        
+        foods       = []
+        idx         = 0
+
+        while len(foods)<10 and idx<len(histories):
+            food_id = histories[idx]['food']
+            try:
+                foods.append(Food.objects.get(pk=food_id))
+            except:
+                pass
+            idx += 1
+
+        serializer  = self.serializer_class(foods, many=True)
+        return Response(serializer.data, status=HTTP_200_OK)
+
+class InterestUserRecommend(APIView):
+    serializer_class    = FoodListSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated, )
+
+    @swagger_auto_schema(   
+        operation_id            = '음식 추천 - 유저/선호대분류 기반',
+        operation_description   = '같은 선호대분류를 선택한 유저들의 인기 음식을 추천합니다.',
+        responses               = {200: openapi.Response('', FoodListSerializer)}
+    )
+    def get(self, request):
+        users       = User.objects.select_related(
+            'profile__interest_in'
+        ).filter(
+            profile__interest_in=request.user.profile.interest_in
+        )
+
+        histories   = History.objects.values(
+            'food'
+        ).filter(
+            user__in= users
+        ).annotate(
+            count=Count('id')
+        ).order_by('-count')
+
         foods       = []
         idx         = 0
         
